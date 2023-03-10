ba527f38ae85acd37ee07af07e5b9b1a4dac4980
Bloan0908 <125679333+Bloan0908@users.noreply.github.com>
2023-03-10 22:59:43 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+a=st.number_input('Tham số a')
+b=st.number_input('Tham số b')
+st.title('GIẢI PHƯƠNG TRÌNH BẬC NHẤT')
+st.balloons()
+if st.button('Giải'):
+  st.write('Phương trình có 1 nghiệm',-b/a)
