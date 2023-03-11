41dbae0a4cd091760611fd5bcad204156dd61e6c
Bloan0908 <125679333+Bloan0908@users.noreply.github.com>
2023-03-11 10:48:38 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import streamlit as st
-import sklearn
+import sklearn #thu vien sklearn luu day du la scikit-learn
 import plotly
 a=st.number_input('Tham số a')
 b=st.number_input('Tham số b')
 st.title('GIẢI PHƯƠNG TRÌNH BẬC NHẤT')
 st.balloons()
 if st.button('Giải'):
-  st.write('Phương trình có 1 nghiệm',-b/a)
+  if (a==0 && b==0): st.write('Phuong trinh co vo so nghiem')
+  if (a==0 && b><0): st.write('Phuong trinh vo nghiem')
+  else: st.write('Phương trình có 1 nghiệm',-b/a)
